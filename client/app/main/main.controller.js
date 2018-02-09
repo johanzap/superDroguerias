a770d71f08bf047e9b5b8a9a9989d4ed0594b9df
Carlos Andres Benavides <cabenavidesd@misena.edu.co>
2018-02-09 17:27:01 -0500

    Se termina la section D y E

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -48,7 +48,8 @@ class MainController {
         laboratory:"Germa66.ltda"
       },
       {
-        imageiption:"Lorem ipsum tatra la la",
+        imageUrl:"assets/images/medicamento.jpg",
+        description:"Lorem ipsum tatra la la",
         price:100000,
         laboratory:"Germa66.ltda"
       },
@@ -87,6 +88,21 @@ class MainController {
         price:100000,
         laboratory:"Germa66.ltda"
       }];
+
+      this.emotions=[
+      {
+        imageUrl:"assets/images/medicamento.jpg",
+        description:"La mejor atención personalizada inmediata"
+      },{
+        imageUrl:"assets/images/medicamento.jpg",
+        description:"Revisión diaria del producto con respecto al fisíco"
+      },{
+        imageUrl:"assets/images/medicamento.jpg",
+        description:"Afiliados a Asocurologos y Copdrogas"
+      },{
+        imageUrl:"assets/images/medicamento.jpg",
+        description:"Contamos con óptimas instalaciones locativas"
+      }]
   }
 }
 
