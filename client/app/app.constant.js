ae0c5395eda3f946f6510402cc583c7de31ea617
JohanVicente <yvmolina19@misena.edu.co>
2018-02-09 08:13:11 -0500

    Se crea el repositorio y se sube el proyecto

---
+++ client/app/app.constant.js
@@ -0,0 +1,13 @@
+(function(angular, undefined) {
+  angular.module("superDrogueriasApp.constants", [])
+
+.constant("appConfig", {
+	"userRoles": [
+		"guest",
+		"user",
+		"admin"
+	]
+})
+
+;
+})(angular);
